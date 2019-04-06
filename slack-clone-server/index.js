fc27e7c1226d31daf10fe27d45a5e1e9b084fa49
HR <hrui1990@gmail.com>
2019-04-06 16:24:00 -0500

    sequelize

--- slack-clone-server/index.js
+++ slack-clone-server/index.js
@@ -4,11 +4,15 @@ import { ApolloServer, gql } from "apollo-server-express";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 
+import models from './models';
+
 const app = express();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
-app.listen({ port: 8080 }, () =>
-  console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`)
-);
+models.sequelize.sync().then(() => {
+  app.listen({ port: 8080 }, () =>
+    console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`)
+  );
+});
