9bf9cfb3de0a1e45b863e2a38cbb7fdc9cc51699
HR <hrui1990@gmail.com>
2019-04-07 13:15:19 -0500

    add front end

--- slacker-server/index.js
+++ slacker-server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import path from 'path';
+
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
 import models from './models';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, './resolvers'))
+);
 
 const app = express();
 
